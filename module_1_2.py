546a0f95cf9758a67000f1d10f2a5c7f34454a93
lunaro-4 <pavelike@gmail.com>
2024-11-28 11:08:58 +0000

    Module 1: task2

---
+++ module_1_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+homeworks : int = 12
+hours_spent : float = 1.5
+course_name : str = 'Python'
+time_spent_per_task : float = hours_spent / homeworks
+
+# print("Домашних заданий сделано", homeworks, "\nВремени затрачено (в часах)", hours_spent, "\nНазвание курса: ", course_name, "\nсреднее время выполнения: ", time_spent_per_task)
+
+
+print("Курс: ", course_name, ", затрачено часов: ", hours_spent, ", среднее время выполнения: ", time_spent_per_task, ".", sep='')
+
+
