f227df81f09255ae7809628fcc304c7d786db460
lunaro-4 <pavelike@gmail.com>
2024-12-10 12:02:11 +0000

    Module 2(additional task): Исправления по обратной связи

--- Module_2/Дополнительное_практическое_задание_по_модулю.py
+++ Module_2/Дополнительное_практическое_задание_по_модулю.py
@@ -1,19 +1,18 @@
-# Функция также добавляет пару '11' для четных чисел,
-# хотя в примере этого не было
+num = int(input("Введите число: "))
+
+
 def generate_password(number: int) -> list:
     password: list = []
     for i in range(1, number):
         for j in range(i, number):
             ij_summ = i + j
             if number % ij_summ == 0:
+                if i == j:
+                    continue
                 password.append(str(i))
                 password.append(str(j))
-    # print(''.join(password))
     return password
 
 
-print(''.join(generate_password(6)))
-print(''.join(generate_password(7)))
-print(''.join(generate_password(8)))
-print(''.join(generate_password(9)))
-print(''.join(generate_password(19)))
+password = generate_password(num)
+print(''.join(password))
