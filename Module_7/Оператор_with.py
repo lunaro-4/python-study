8b4ea74ed06ddf7709f49fcd3151866ae1c9237f
lunaro <pavelike@gmail.com>
2025-01-23 11:46:01 +0300

    Module 7(Task 3): 'Оператор with'

---
+++ Module_7/Оператор_with.py
@@ -0,0 +1,49 @@
+class WordsFinder:
+    def __init__(self, *files) -> None:
+        self.file_names = list(files)
+
+    def __clean_line(self, line: str) -> str:
+        clean_line: str = line
+        for symbol in [',', '.', '=', '!', '?', ';', ':', ' - ', '\n']:
+            clean_line = clean_line.replace(symbol, '')
+        return clean_line.casefold()
+
+    def get_all_words(self) -> dict[str, list[str]]:
+        all_words_dict: dict[str, list[str]] = {}
+        for file_name in self.file_names:
+            word_list: list[str] = []
+            with open(file_name, 'r') as file:
+                for line in file.readlines():
+                    word_list.extend(self.__clean_line(line).split(' '))
+            all_words_dict[file_name] = word_list
+        return all_words_dict
+    
+    def find(self, word: str) -> dict[str, int]:
+        file_to_words: dict[str, int] = {}
+        all_words: dict[str, list[str]] = self.get_all_words()
+        for file, word_list in all_words.items():
+            word_index: int = word_list.index(word.casefold())
+            if word_index != -1:
+                file_to_words[file] = word_index +1
+        return file_to_words
+
+    def count(self, word: str) -> dict[str, int]:
+        file_to_words: dict[str, int] = {}
+        all_words: dict[str, list[str]] = self.get_all_words()
+        for file, word_list in all_words.items():
+            word_count: int = word_list.count(word.casefold())
+            if word_count != -1:
+                file_to_words[file] = word_count
+        return file_to_words
+
+
+
+def main():
+    finder2 = WordsFinder('test_file.txt')
+    print(finder2.get_all_words()) # Все слова
+    print(finder2.find('TEXT')) # 3 слово по счёту
+    print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+
+if __name__ == "__main__":
+    main()
