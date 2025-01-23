9002ce2d2c1bceb00097c35436ab5693eba13a0c
lunaro <pavelike@gmail.com>
2025-01-23 10:46:12 +0300

    Module 7(Task 2): 'Позиционирование в файле'

---
+++ Module_7/Позиционирование_в_файле.py
@@ -0,0 +1,25 @@
+def custom_write(file_name: str, strings: list[str]) -> dict:
+    position_to_string: dict = {}
+    
+    file = open(file_name, 'w')
+    for i, string in enumerate(strings):
+        byte_pos: int = file.tell()
+        index_tuple: tuple = (i, byte_pos)
+        file.write(string + '\n')
+        position_to_string[index_tuple] = string
+    file.tell()
+    return position_to_string
+
+def main():
+    info = [
+            'Text for tell.',
+            'Используйте кодировку utf-8.',
+            'Because there are 2 languages!',
+            'Спасибо!'
+            ]
+    result = custom_write('test.txt', info)
+    for elem in result.items():
+        print(elem)
+
+if __name__ == "__main__":
+    main()
